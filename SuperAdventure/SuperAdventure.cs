9e50f8ba80fe8073635134bbda26357c4da89fdf
Jufebrown <jufebrown@gmail.com>
2018-10-31 13:53:51 -0500

    add constructor for player

--- SuperAdventure/SuperAdventure.cs
+++ SuperAdventure/SuperAdventure.cs
@@ -7,14 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Engine;
 
 namespace SuperAdventure
 {
     public partial class SuperAdventure : Form
     {
+        private Player _player;
         public SuperAdventure()
         {
             InitializeComponent();
+
+            _player = new Player();
+
+            _player.CurrentHitPoints = 10;
+            _player.MaxHitPoints = 10;
+            _player.Gold = 30;
+            _player.ExpPoints = 0;
+            _player.Level = 1;
+
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExperience.Text = _player.ExpPoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
         }
 
         private void SuperAdventure_Load(object sender, EventArgs e)
