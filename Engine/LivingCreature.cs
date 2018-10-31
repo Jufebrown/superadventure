59be1bc683209ddb965b6feb6ee08a242feddf43
Jufebrown <jufebrown@gmail.com>
2018-10-31 14:40:35 -0500

    add LivingCreature class

---
+++ Engine/LivingCreature.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine
+{
+    public class LivingCreature
+    {
+        public int MaxHitPoints { get; set; }
+        public int CurrentHitPoints { get; set; }
+    }
+}
