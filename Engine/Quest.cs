f6deb84bafd6e9b8a22467ccc79394fd48c286ed
Jufebrown <jufebrown@gmail.com>
2018-10-31 14:16:00 -0500

    add Quest class

---
+++ Engine/Quest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine
+{
+    class Quest
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int RewardXP { get; set; }
+        public int RewardGold { get; set; }
+    }
+}
