2b3d3aec427e3acfdeb7c06aebb4c18fe92257ca
Jufebrown <jufebrown@gmail.com>
2018-10-31 16:02:32 -0500

    add constructor to monster class

--- Engine/Monster.cs
+++ Engine/Monster.cs
@@ -8,8 +8,17 @@ namespace Engine
     {
         public int ID { get; set; }
         public string Name { get; set; }
-        public int MaximumDamage { get; set; }
+        public int MaxDamage { get; set; }
         public int RewardXP { get; set; }
         public int RewardGold { get; set; }
+
+        public Monster(int id, string name, int maxDamage, int rewardXP, int rewardGold, int currentHP, int maxHP) : base(currentHP, maxHP)
+        {
+            ID = id;
+            Name = name;
+            MaxDamage = maxDamage;
+            RewardXP = rewardXP;
+            RewardGold = rewardGold;
+        }
     }
 }
