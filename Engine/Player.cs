53ccd0fc1b658776f636644c04c266604051c7f4
Jufebrown <jufebrown@gmail.com>
2018-10-31 13:15:56 -0500

    add player class to engine

---
+++ Engine/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine
+{
+    public class Player
+    {
+        public int CurrentHitPoints { get; set; }
+        public int MaxHitPoints { get; set; }
+        public int Gold { get; set; }
+        public int ExpPoints { get; set; }
+        public int Level { get; set; }
+    }
+}
